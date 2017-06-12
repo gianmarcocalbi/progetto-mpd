868e8fb782cbd0e09a9694b0c77e048a198f88b4
Davide <d.merletti@campus.unimib.it>
2017-06-12 17:05:55 +0200

    -Sostituiti gli spazi e tab con ";" -Splittamento della stringa e caricamento in un array

--- align.py
+++ align.py
@@ -1,9 +1,19 @@
 import re
 
-f = open("C:/Users/Davide/Desktop/Modelli probabilistici - Progetto/OrdonezA_Sensors.txt","r")
+f = open("C:/Users/Davide/Desktop/Modelli probabilistici - Progetto/OrdonezA_Sensors.txt", "r")
+no_blank = open("C:/Users/Davide/Desktop/Modelli probabilistici - Progetto/no_blank.txt", "w")
+c = [];
 count = 0
 for line in f:
-    if (count != 0 and count != 1):
-        re.sub("\\s+",";",line)
-        print(line)
+    if count != 0 and count != 1:
+        line = re.sub("\\s+", ";", line)
+        line = line[:-1]
+        #print(line)
+        m = line.split(sep=";")
+        #print(m)
+        no_blank.write(line + '\n')
+        c.append(m)
+        print(c)
+    count += 1
 f.close()
+no_blank.close()
