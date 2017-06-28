bd8bd39ff3d421a12c54dd8444990ae7171ac5e4
nicoperillo <n.perillo@campus.unimib.it>
2017-06-28 20:23:15 +0200

    Fix
    - Restored datetime format
    - Changed local paths
    - Aligned both txts
    - Restored original txt files (one was wrong)

--- align.py
+++ align.py
@@ -1,19 +1,33 @@
 import re
 
-f = open("C:/Users/Davide/Desktop/Modelli probabilistici - Progetto/OrdonezA_Sensors.txt", "r")
-no_blank = open("C:/Users/Davide/Desktop/Modelli probabilistici - Progetto/no_blank.txt", "w")
+f = open("inc/OrdonezA_Sensors.txt", "r")
+f1 = open("inc/OrdonezA_ADLs.txt", "r")
+no_blankS = open("no_blankSensors.txt", "w")
+no_blankA = open("no_blankActivities.txt", "w")
 c = []
+c1 = []
 count = 0
 for line in f:
     if count != 0 and count != 1:
-        line = re.sub("\\s+", ";", line)
+        line = re.sub("\\t+", ";", line)
         line = line[:-1]
-        # print(line)
         m = line.split(sep=";")
-        # print(m)
-        no_blank.write(line + '\n')
+        no_blankS.write(line + ';\n')
         c.append(m)
-        print(c)
     count += 1
+
+count = 0
+for line in f1:
+    if count != 0 and count != 1:
+        line = re.sub("\\t+", ";", line)
+        line = line[:-1]
+        m = line.split(sep=";")
+        no_blankA.write(line + '\n')
+        c1.append(m)
+    count += 1   
+    
+print('Done.')
 f.close()
-no_blank.close()
+f1.close()
+no_blankS.close()
+no_blankA.close()
