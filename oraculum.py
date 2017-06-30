a29f00a3c02c85ecdf6d455d69aa189f60d94d00
grimidev <calbigianmarco@gmail.com>
2017-06-30 15:11:05 +0000

    Added working Viterbi algorithm :)

--- oraculum.py
+++ oraculum.py
@@ -2,62 +2,111 @@ from import_me import *
 
 
 class Oraculum:
-    obs, states, start_p, trans_p, emit_p
+    obs = states = start_p = trans_p = emit_p = None
     def __init__(self):
         obs = list(OBS_DICT.values())
         states = list(ACT_DICT.values())
         
-        with open("/refined_data/OrdonezA_refined_1498809985_start_probs.json") as f:
-            start_p = json.load(f)
-        
-        with open("/refined_data/OrdonezA_refined_1498809985_trans_matrix.json") as f:
-            trans_p = json.load(f)
+        with open("./refined_data/OrdonezA_refined_start_probs.json") as f:
+            start_p = numpy.asarray(json.load(f))
             
-        emit_p = {
-           'Healthy' : {'normal': 0.5, 'cold': 0.4, 'dizzy': 0.1},
-           'Fever' : {'normal': 0.1, 'cold': 0.3, 'dizzy': 0.6}
+        with open("./refined_data/OrdonezA_refined_trans_matrix.json") as f:
+            trans_p = numpy.asarray(json.load(f))
+            
+        with open("./refined_data/OrdonezA_refined_emiss_matrix.json") as f:
+            emiss_p = json.load(f)
+
+    def viterbi(self, prior, given_obs, trans_mat, obs_mat):
+        # given_obs = [#1, #2, #3, #4, #5]   0  <= given_obs[i]  <= (2^12)-1
+        #              t1  t2  t3  t4  t5
+        
+        most_likely_mat = []    #da rimepire con gli indici j
+        most_likely_seq = []    #da riempire col massimo valore corrispondente alla colonna t
+        
+        prev_p = []
+        
+        for t in range(0,len(given_obs)):
+            most_likely_mat.append([])
+            #passo 0
+            if t==0:
+                for i in range(len(prior)):
+                    if given_obs[0] in obs_mat[i]:
+                        prev_p.append(prior[i] * obs_mat[i][given_obs[0]])
+                    else:
+                        prev_p.append(0)
+            else:
+                p = []
+                
+                # passo iterativo
+                # per ogni azione (cioè per ogni stato dell'HMM)
+                for i in range(len(prior)):
+                    # i -> indice che identifica lo stato corrente (in cui arrivo)
+                    max_p = 0
+                    max_j = 0
+                    
+                    # se l'osservazione corrente non è presente come chiave nella
+                    # matrice sparsa delle osservazioni, vuol dire che la prob
+                    # di osservarla nello stato corrente è uguale a 0, quindi devp
+                    # considerare solo i casi in cui tale probabilità è invece > 0
+                    # ovvero quanto l'osservazione è presente come chiave nella matr
+                    # delle osservazioni
+                    if given_obs[t] in obs_mat[i]:
+                        # prob di osservare l'osservazione corrente given_obs[t]
+                        # nello stato i-esimo
+                        # obs_mat[i][given_obs[t]] = P(i|E)
+                        curr_state_prob = obs_mat[i][given_obs[t]]
+                    
+                        # però posso arrivare in questo stato da 12 stati diversi
+                        for j in range(len(prior)):
+                            # j -> indice che identifica lo stato dal quale arrivo
+                            
+                            # per ognuno dei 12 stati dai quali posso arrivare devo moltiplicare curr_state_prob
+                            # per la prob di arrivare nello stato corrente dallo stato j-esimo per la prob dello
+                            # stato di partenza
+                            # trans_mat[i][j] = P(i|j)
+                            # prev_p[j] = P(j)
+                            curr_p = curr_state_prob * prev_p[j] * trans_mat[j][i]
+                            
+                            # se la prob trovata è massima per lo stato i-esimo allora la setto
+                            # come prob massima corrente
+                            if curr_p > max_p:
+                                max_p = curr_p
+                                #salvo l'indice di arrivo del massimo
+                                max_j = j
+                        
+                    p.append(max_p)
+                    most_likely_mat[t].append(max_j)
+                prev_p = p
+        
+        most_likely_seq.append(prev_p.index(max(prev_p)))
+        
+        t = len(most_likely_mat)-1;
+        k = 0
+        while t > 0:
+            most_likely_seq.append(most_likely_mat[t][most_likely_seq[k]])
+            t-=1
+            k+=1
+            
+        return most_likely_seq
+
+
+if __name__ == '__main__':
+    oracle = Oraculum()
+    print(oracle.viterbi(
+        [0.5, 0.5],
+        ["U", "U", "notU", "U", "U"],
+        [
+            [0.7, 0.3],
+            [0.3, 0.7]
+        ],
+        {
+            0 : {
+                "U" : 0.9,
+                "notU" : 0.1
+            },
+            1 : {
+                "U": 0.2,
+                "notU" : 0.8
+            }
         }
-
-    def viterbi(obs, states, start_p, trans_p, emit_p):
-        V = [{}]
-        for st in states:
-            V[0][st] = {"prob": start_p[st] * emit_p[st][obs[0]], "prev": None}
-        # Run Viterbi when t > 0
-        for t in range(1, len(obs)):
-            V.append({})
-            for st in states:
-                max_tr_prob = max(V[t-1][prev_st]["prob"]*trans_p[prev_st][st] for prev_st in states)
-                for prev_st in states:
-                    if V[t-1][prev_st]["prob"] * trans_p[prev_st][st] == max_tr_prob:
-                        max_prob = max_tr_prob * emit_p[st][obs[t]]
-                        V[t][st] = {"prob": max_prob, "prev": prev_st}
-                        break
-        for line in dptable(V):
-            print(line)
-        opt = []
-        # The highest probability
-        max_prob = max(value["prob"] for value in V[-1].values())
-        previous = None
-        # Get most probable state and its backtrack
-        for st, data in V[-1].items():
-            if data["prob"] == max_prob:
-                opt.append(st)
-                previous = st
-                break
-        # Follow the backtrack till the first observation
-        for t in range(len(V) - 2, -1, -1):
-            opt.insert(0, V[t + 1][previous]["prev"])
-            previous = V[t + 1][previous]["prev"]
-     
-        print 'The steps of states are ' + ' '.join(opt) + ' with highest probability of %s' % max_prob
-     
-    def dptable(V):
-        # Print a table of steps from dictionary
-        yield " ".join(("%12d" % i) for i in range(len(V)))
-        for state in V[0]:
-            yield "%.7s: " % state + " ".join("%.7s" % ("%f" % v[state]["prob"]) for v in V)
-
-    
-
-   
- 
+    ))
